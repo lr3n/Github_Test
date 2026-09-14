7672ad85e924ebf87b7edc2b6186c3449c29f24f
Ben Gerard B. Rosete <ben@gmail.com>
2026-09-15 03:55:29 +0800

    Test Version

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$name = "BSCpE 4B";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Sample</title>
+</head>
+<body>
+
+    <h1>Hello, <?php echo $name; ?>!</h1>
+    <p>Version 1 of our project</p>
+
+</body>
+</html>
